d41712120f00267cde1a5d1e74f9f5ec83c2e657
JosuePaiva02 <u202119095@upc.edu.pe>
2026-05-22 15:32:18 -0500

    feat: finished setting up home as default view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './shared/presentation/components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'finca-clientes';
+  title = 'finca-frontend';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+const baseTitle = 'Finca Verde';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/presentation/components/layout/home-component/home-component.component')
+        .then(m => m.HomeComponentComponent),
+    title: `${baseTitle} - Home`
+  },
+];
